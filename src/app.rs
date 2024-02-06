0c48ce6be15f3d4c9609f1902ab46267d550907c
Sebastian Hasler <sebastian.hasler@sec.uni-stuttgart.de>
2024-02-06 12:42:54 +0100

    reproducible example for https://github.com/leptos-rs/leptos/issues/2265

--- src/app.rs
+++ src/app.rs
@@ -1,14 +1,26 @@
 use leptos::*;
 use leptos_meta::*;
+use leptos_router::*;
 
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
+
+    let handle_consent = create_server_action::<HandleConsent>();
 
     view! {
         <Stylesheet id="leptos" href="/pkg/leptos-playground.css"/>
         <Title text="Leptos Playground"/>
 
         {"Hello, World!"}
+        <ActionForm action=handle_consent>
+            <button type="submit" name="accept" value="true">Accept</button>
+        </ActionForm>
     }
+}
+
+#[server(HandleConsent)]
+async fn handle_consent(accept: Option<String>) -> Result<(), ServerFnError> {
+    println!("{:?}", accept);
+    Ok(())
 }
