9f0965b513214d489214e73cfbbf1818a97b51f1
Sebastian Hasler <sebastian.hasler@sec.uni-stuttgart.de>
2024-02-06 12:28:59 +0100

    initial commit
    - Generated project via:
      LEPTOS_CARGO_GENERATE_VERSION=v0.19.0 cargo leptos new --git leptos-rs/start-axum
    - Added flake.nix for dev shell

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "A basic Rust devshell for NixOS users developing wireguard-self-service";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell {
+          buildInputs = [
+            binaryen
+            cargo-leptos
+            dart-sass
+            leptosfmt
+            (rust-bin.selectLatestNightlyWith( toolchain: toolchain.default.override {
+              extensions= [ "rust-src" "rust-analyzer" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            }))
+          ] ++ pkgs.lib.optionals pkg.stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+        };
+      }
+    );
+}
